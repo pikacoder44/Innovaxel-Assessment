898fcb59358f50721ce1881e4e77f8ca71a1f27d
pikacoder44 <pikaop125@gmail.com>
2025-09-25 09:37:18 +0500

    Updated Readme.md

--- backend/rooms.js
+++ backend/rooms.js
@@ -1,7 +1,7 @@
 const rooms = {
   ServerRoom: { minLevel: 2, open: "09:00", close: "11:00", cooldown: 15 },
   Vault: { minLevel: 3, open: "09:00", close: "10:00", cooldown: 30 },
-  "R&D-Lab": { minLevel: 1, open: "08:00", close: "12:00", cooldown: 10 },
+  "R&D Lab": { minLevel: 1, open: "08:00", close: "12:00", cooldown: 10 },
 };
 
 module.exports = rooms;
